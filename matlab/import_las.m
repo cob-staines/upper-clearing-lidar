4993c8725c7de867f75d73422fe18b873def4d0b
unknown <cob.staines@gmail.com>
2019-09-30 16:19:27 -0600

    prelimitany additions to las_read.m

---
+++ matlab/import_las.m
@@ -0,0 +1,5 @@
+data_dir = 'C:/Users/Cob/Documents/educational/usask/research/data/LiDAR/esc/19_052_a_2_120m_grid_WGS84_utm_N11_nocolor - Scanner 1 - 190221_181450_Scanner_1 - originalpoints.las';
+
+data = lasdata(data_dir, 'loadall');
+
+%%
