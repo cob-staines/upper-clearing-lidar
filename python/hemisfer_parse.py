7753c1b6d44af1511372327825296efd5a717f1a
unknown <cob.staines@gmail.com>
2020-08-31 18:33:31 -0600

    added to hemisfer_parse to allow for threshold set by algorithms, hemi_optimization_2 now includes analysis of rmse of lidar by lai_method and threshold method

--- python/hemisfer_parse.py
+++ python/hemisfer_parse.py
@@ -3,11 +3,27 @@ import pandas as pd
 
 rx_dict = {
     'picture': re.compile(r'picture\s*(?P<picture>.*)\n'),
-    'threshold': re.compile(r'threshold\s*(?P<threshold>\d+)\n'),
-    'ringcount_ringwidth': re.compile(r'rings\s*(?P<ringcount>\d+)\s*(?P<ringwidth>\d+,\d+).*\n'),
+    'threshold': re.compile(r'threshold\s*(?P<threshold>\S+).*\n'),
+    'ringcount_ringwidth': re.compile(r'rings\s*(?P<ringcount>\d+)\s*(?P<ringwidth_deg>\d+,\d+).*\n'),
+    'ring_table': re.compile(r'ring\tangle.*width\n'),
     'transmission': re.compile(r'transmission\s*(?P<transmission>.*)%\n'),
     'openness': re.compile(r'openness\s*(?P<openness>.*)%\n'),
     'licor_lai': re.compile(r'LiCor LAI2000\s*(?P<lai_no_cor>\d+,\d+)\s*(?P<laa_no_cor>\d+)\s*(?P<lai_s>\d+,\d+)\s*(?P<laa_s>\d+)\s*(?P<lai_cc>\d+,\d+)\s*(?P<laa_cc>\d+)\s*(?P<lai_s_cc>\d+,\d+)\s*(?P<laa_s_cc>\d+)\n')
+}
+
+rx_conditional = {
+    'transmission_gaps': re.compile(r'\sgaps\s*(?P<transmission_gaps>.*)%\n'),
+    'openness_gaps': re.compile(r'\sgaps\s*(?P<openness_gaps>.*)%\n'),
+    'ring_1': re.compile(r'1\s\S*\s\S*\s\S*\s(?P<transmission_1>\S*)\s(?P<transmission_s_1>\S*)\s(?P<contactnum_1>\S*)\s(?P<gaps_1>\S*)\s\S*\s\S*'),
+    'ring_2': re.compile(r'2\s\S*\s\S*\s\S*\s(?P<transmission_2>\S*)\s(?P<transmission_s_2>\S*)\s(?P<contactnum_2>\S*)\s(?P<gaps_2>\S*)\s\S*\s\S*'),
+    'ring_3': re.compile(r'3\s\S*\s\S*\s\S*\s(?P<transmission_3>\S*)\s(?P<transmission_s_3>\S*)\s(?P<contactnum_3>\S*)\s(?P<gaps_3>\S*)\s\S*\s\S*'),
+    'ring_4': re.compile(r'4\s\S*\s\S*\s\S*\s(?P<transmission_4>\S*)\s(?P<transmission_s_4>\S*)\s(?P<contactnum_4>\S*)\s(?P<gaps_4>\S*)\s\S*\s\S*'),
+    'ring_5': re.compile(r'5\s\S*\s\S*\s\S*\s(?P<transmission_5>\S*)\s(?P<transmission_s_5>\S*)\s(?P<contactnum_5>\S*)\s(?P<gaps_5>\S*)\s\S*\s\S*'),
+    'ring_th_1': re.compile(r'1\s\S*\s(?P<threshold_1>\S*)\s\S*\s\S*\s(?P<transmission_1>\S*)\s(?P<transmission_s_1>\S*)\s(?P<contactnum_1>\S*)\s(?P<gaps_1>\S*)\s\S*\s\S*'),
+    'ring_th_2': re.compile(r'2\s\S*\s(?P<threshold_2>\S*)\s\S*\s\S*\s(?P<transmission_2>\S*)\s(?P<transmission_s_2>\S*)\s(?P<contactnum_2>\S*)\s(?P<gaps_2>\S*)\s\S*\s\S*'),
+    'ring_th_3': re.compile(r'3\s\S*\s(?P<threshold_3>\S*)\s\S*\s\S*\s(?P<transmission_3>\S*)\s(?P<transmission_s_3>\S*)\s(?P<contactnum_3>\S*)\s(?P<gaps_3>\S*)\s\S*\s\S*'),
+    'ring_th_4': re.compile(r'4\s\S*\s(?P<threshold_4>\S*)\s\S*\s\S*\s(?P<transmission_4>\S*)\s(?P<transmission_s_4>\S*)\s(?P<contactnum_4>\S*)\s(?P<gaps_4>\S*)\s\S*\s\S*'),
+    'ring_th_5': re.compile(r'5\s\S*\s(?P<threshold_5>\S*)\s\S*\s\S*\s(?P<transmission_5>\S*)\s(?P<transmission_s_5>\S*)\s(?P<contactnum_5>\S*)\s(?P<gaps_5>\S*)\s\S*\s\S*'),
 }
 
 def _parse_line(line):
@@ -39,10 +55,9 @@ def parse_file(file_in, file_out):
         Parsed data
 
     """
-
     data = []  # create an empty list to collect the data
     # open the file and read through it line by line
-    # file_object = open(filepath, 'r')
+    # file_object = open(file_in, 'r')
     with open(file_in, 'r') as file_object:
         # preallocate empty entry
         entry = {}
@@ -62,8 +77,28 @@ def parse_file(file_in, file_out):
                     # add group and value to entry
                     entry.update({list(match.re.groupindex.keys())[ii]: match.group(list(match.re.groupindex.keys())[ii])})
 
-                # at the last trigger key
-                if key == 'licor_lai':
+                # conditional lines
+                if key == 'transmission':
+                    line = file_object.readline()
+                    match = rx_conditional['transmission_gaps'].search(line)
+                    entry.update({list(match.re.groupindex.keys())[0]: match.group(list(match.re.groupindex.keys())[0])})
+                elif key == 'openness':
+                    line = file_object.readline()
+                    match = rx_conditional['openness_gaps'].search(line)
+                    entry.update({list(match.re.groupindex.keys())[0]: match.group(list(match.re.groupindex.keys())[0])})
+                elif key == 'ring_table':
+                    for jj in range(1, 6):
+                        line = file_object.readline()
+                        if entry['threshold'] == 'rings':
+                            key = 'ring_th_' + str(jj)
+                        else:
+                            key = 'ring_' + str(jj)
+                        match = rx_conditional[key].search(line)
+                        for ii in range(0, match.re.groups):
+                            # add group and value to entry
+                            entry.update({list(match.re.groupindex.keys())[ii]: match.group(
+                                list(match.re.groupindex.keys())[ii])})
+                elif key == 'licor_lai':  # at the last trigger key
                     # append entry to data
                     data.append(entry)
                     # clear entry for next round
@@ -77,15 +112,20 @@ def parse_file(file_in, file_out):
     # replace commas with decimals
     data = data.replace(',', '.', regex=True)
     # convert all columns except for first to numeric
-    data.iloc[:, 1:] = data.iloc[:, 1:].apply(pd.to_numeric, errors='coerce')
+    # data.iloc[:, 1:] = data.iloc[:, 1:].apply(pd.to_numeric, errors='coerce')
+    # convert transmissions to from percent to decimal
+    data[['transmission', 'transmission_gaps', 'openness', 'openness_gaps']] = data[['transmission', 'transmission_gaps', 'openness', 'openness_gaps']].astype('float') / 100
 
     # write output to file
     data.to_csv(file_out, index=False)
 
     return data
 
-file_in = "C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\synthetic_hemis\\opt\\poisson\\LAI.dat"
-file_out = file_in.replace('LAI.dat', 'LAI_parsed.csv')
+# file_in = "C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\synthetic_hemis\\opt\\poisson\\LAI.dat"
+# file_out = file_in.replace('LAI.dat', 'LAI_parsed.csv')
+
+file_in = "C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\hemispheres\\19_149\\clean\\sized\\LAI_rc_rings.dat"
+file_out = file_in.replace('.dat', '_parsed.dat')
 
 # with open(file_in) as file:
 #     file_contents = file.read()
--- r/optimization/hemi_optimization_2.r
+++ r/optimization/hemi_optimization_2.r
@@ -2,7 +2,7 @@ library('dplyr')
 library('tidyr')
 library('ggplot2')
 
-photos_lai_in = "C:/Users/Cob/index/educational/usask/research/masters/data/hemispheres/19_149/clean/sized/LAI_parsed.csv"
+photos_lai_in = "C:/Users/Cob/index/educational/usask/research/masters/data/hemispheres/19_149/clean/sized/LAI_manual_parsed.dat"
 photos_lai = read.csv(photos_lai_in, header=TRUE, na.strings = c("NA",""), sep=",")
 photos_lai$original_file = gsub("_r.JPG", ".JPG", photos_lai$picture)
 photos_meta_in = "C:/Users/Cob/index/educational/usask/research/masters/data/hemispheres/hemi_lookup_cleaned.csv"
@@ -11,7 +11,7 @@ photos_meta$id <- as.numeric(rownames(photos_meta)) - 1
 
 photos = merge(photos_lai, photos_meta, by.x='original_file', by.y='filename', all.x=TRUE)
 
-synth_lai_in = "C:/Users/Cob/index/educational/usask/research/masters/data/lidar/synthetic_hemis/opt/poisson/LAI_parsed.csv"
+synth_lai_in = "C:/Users/Cob/index/educational/usask/research/masters/data/lidar/synthetic_hemis/opt/poisson/LAI_parsed.dat"
 synth_lai = read.csv(synth_lai_in, header=TRUE, na.strings = c("NA",""), sep=",")
 synth_meta_in = "C:/Users/Cob/index/educational/usask/research/masters/data/lidar/synthetic_hemis/opt/poisson/hemimetalog.csv"
 synth_meta = read.csv(synth_meta_in, header=TRUE, na.strings = c("NA",""), sep=",")
@@ -24,7 +24,13 @@ all = merge(synth, photos, by='id', all.x=TRUE, suffixes=c("_synth", "_photo"))
 # calculate errors
 all = all %>%
   mutate(lai_no_cor_error = lai_no_cor_synth - lai_no_cor_photo) %>%
-  mutate(lai_s_cc_error = lai_s_cc_synth - lai_s_cc_photo)
+  mutate(lai_s_cc_error = lai_s_cc_synth - lai_s_cc_photo) %>%
+  mutate(co_error = openness_synth - openness_photo) %>%
+  mutate(co_error_1 = transmission_s_1_synth - transmission_s_1_photo) %>%
+  mutate(co_error_2 = transmission_s_2_synth - transmission_s_2_photo) %>%
+  mutate(co_error_3 = transmission_s_3_synth - transmission_s_3_photo) %>%
+  mutate(co_error_4 = transmission_s_4_synth - transmission_s_4_photo) %>%
+  mutate(co_error_5 = transmission_s_5_synth - transmission_s_5_photo)
 
 rmse = function(difdata){
   sqrt(sum(difdata^2, na.rm = TRUE))/sqrt(length(na.omit(difdata)))
@@ -34,23 +40,81 @@ mae = function(difdata){
   sum(abs(difdata), na.rm = TRUE)/length(na.omit(difdata))
 }
 
+# plots
+
+# optimized LAI
 all_agg = all %>%
   group_by(poisson_radius_m, optimization_scalar) %>%
-  summarise(rmse_lai = rmse(lai_s_cc_error), mae_lai = mae(lai_s_cc_error), mean_bias_lai = mean(lai_s_cc_error, na.rm = TRUE) , n = length(na.omit(lai_s_cc_error)))
-
+  summarise(rmse_lai_s_cc = rmse(lai_s_cc_error), mae_lai_s_cc = mae(lai_s_cc_error), mean_bias_lai_s_cc = mean(lai_s_cc_error, na.rm = TRUE),
+            rmse_co = rmse(co_error), mae_co = mae(co_error), mean_bias_co = mean(co_error, na.rm = TRUE))
 all_agg$poisson_radius_m = as.factor(all_agg$poisson_radius_m)
 
-# plot
+# mean bias lai
+ggplot(all_agg, aes(x=optimization_scalar, y=mean_bias_lai_s_cc, color=poisson_radius_m)) +
+  geom_point() +
+  geom_line() +
+  xlim(0, 1)
 
-# rmse
-ggplot(all_agg, aes(x=optimization_scalar, y=rmse_lai, color=poisson_radius_m)) +
+# rmse lai
+ggplot(all_agg, aes(x=optimization_scalar, y=rmse_lai_s_cc, color=poisson_radius_m)) +
+  geom_point() +
+  geom_line() +
+  ylim(0, 1)
+
+# mean bias co
+ggplot(all_agg, aes(x=optimization_scalar, y=mean_bias_co, color=poisson_radius_m)) +
   geom_point() +
   geom_line()
 
-# mean bias
-ggplot(all_agg, aes(x=optimization_scalar, y=mean_bias_lai, color=poisson_radius_m)) +
+# rmse co
+ggplot(all_agg, aes(x=optimization_scalar, y=rmse_co, color=poisson_radius_m)) +
   geom_point() +
   geom_line()
+
+
+# plot mean bias of canopy openness over all rings
+all_agg = all %>%
+  group_by(poisson_radius_m, optimization_scalar) %>%
+  summarise(rmse_lai_s_cc = rmse(lai_s_cc_error), mae_lai_s_cc = mae(lai_s_cc_error), mean_bias_lai_s_cc = mean(lai_s_cc_error, na.rm = TRUE),
+            rmse_co = rmse(co_error), mae_co = mae(co_error), mean_bias_co = mean(co_error, na.rm = TRUE),
+            rmse_co_1 = rmse(co_error_1), mae_co_1 = mae(co_error_1), mean_bias_co_1 = mean(co_error_1, na.rm = TRUE),
+            rmse_co_2 = rmse(co_error_2), mae_co_2 = mae(co_error_2), mean_bias_co_2 = mean(co_error_2, na.rm = TRUE),
+            rmse_co_3 = rmse(co_error_3), mae_co_3 = mae(co_error_3), mean_bias_co_3 = mean(co_error_3, na.rm = TRUE),
+            rmse_co_4 = rmse(co_error_4), mae_co_4 = mae(co_error_4), mean_bias_co_4 = mean(co_error_4, na.rm = TRUE),
+            rmse_co_5 = rmse(co_error_5), mae_co_5 = mae(co_error_5), mean_bias_co_5 = mean(co_error_5, na.rm = TRUE),
+            n = length(na.omit(lai_s_cc_error))) %>%
+  gather("mb_group", "mean_bias", mean_bias_co, mean_bias_co_1, mean_bias_co_2, mean_bias_co_3, mean_bias_co_4, mean_bias_co_5)
+all_agg$poisson_radius_m = as.factor(all_agg$poisson_radius_m)
+
+ggplot(all_agg, aes(x=optimization_scalar, y=mean_bias, color=mb_group)) +
+  facet_grid(. ~ poisson_radius_m) +
+  geom_point() +
+  geom_line()
+
+# plot rmse of canopy openness over all rings
+all_agg = all %>%
+  group_by(poisson_radius_m, optimization_scalar) %>%
+  summarise(rmse_lai_s_cc = rmse(lai_s_cc_error), mae_lai_s_cc = mae(lai_s_cc_error), mean_bias_lai_s_cc = mean(lai_s_cc_error, na.rm = TRUE),
+            rmse_co = rmse(co_error), mae_co = mae(co_error), mean_bias_co = mean(co_error, na.rm = TRUE),
+            rmse_co_1 = rmse(co_error_1), mae_co_1 = mae(co_error_1), mean_bias_co_1 = mean(co_error_1, na.rm = TRUE),
+            rmse_co_2 = rmse(co_error_2), mae_co_2 = mae(co_error_2), mean_bias_co_2 = mean(co_error_2, na.rm = TRUE),
+            rmse_co_3 = rmse(co_error_3), mae_co_3 = mae(co_error_3), mean_bias_co_3 = mean(co_error_3, na.rm = TRUE),
+            rmse_co_4 = rmse(co_error_4), mae_co_4 = mae(co_error_4), mean_bias_co_4 = mean(co_error_4, na.rm = TRUE),
+            rmse_co_5 = rmse(co_error_5), mae_co_5 = mae(co_error_5), mean_bias_co_5 = mean(co_error_5, na.rm = TRUE),
+            n = length(na.omit(lai_s_cc_error))) %>%
+  gather("rmse_group", "rmse", rmse_co, rmse_co_1, rmse_co_2, rmse_co_3, rmse_co_4, rmse_co_5)
+all_agg$poisson_radius_m = as.factor(all_agg$poisson_radius_m)
+
+ggplot(all_agg, aes(x=optimization_scalar, y=rmse, color=rmse_group)) +
+  facet_grid(. ~ poisson_radius_m) +
+  geom_point() +
+  geom_line()
+
+# calculation time
+all$poisson_radius_m = as.factor(all$poisson_radius_m)
+ggplot(all, aes(x=optimization_scalar, y=computation_time_s, color=poisson_radius_m)) +
+  geom_point() +
+  ylim(0, 200)
 
 # subset to look at spread
 selection_1 <- all %>%
@@ -63,11 +127,119 @@ selection = rbind(selection_1, selection_2, selection_3)
 
 selection$poisson_radius_m = as.factor(selection$poisson_radius_m)
 
-ggplot(selection, aes(x=lai_no_cor_photo, y=lai_no_cor_synth, color=poisson_radius_m)) + 
+ggplot(selection, aes(x=lai_s_cc_photo, y=lai_s_cc_synth, color=poisson_radius_m)) + 
   geom_point() +
   geom_abline(slope=1, intercept=0)
 
-### BOOKMARK -- everything below is old hat, needs addaptation.
+# Threshold method comparison
+photos_meta_in = "C:/Users/Cob/index/educational/usask/research/masters/data/hemispheres/hemi_lookup_cleaned.csv"
+photos_meta = read.csv(photos_meta_in, header=TRUE, na.strings = c("NA",""), sep=",")
+photos_meta$id <- as.numeric(rownames(photos_meta)) - 1
+photos_meta = photos_meta %>% select('id', 'filename')
+
+
+photos_manual_in = "C:/Users/Cob/index/educational/usask/research/masters/data/hemispheres/19_149/clean/sized/LAI_manual_parsed.dat"
+photos_manual = read.csv(photos_manual_in, header=TRUE, na.strings = c("NA",""), sep=",")
+photos_manual$original_file = gsub("_r.JPG", ".JPG", photos_manual$picture)
+photos_manual = photos_manual %>% select('original_file', 'lai_no_cor', 'lai_s', 'lai_cc', 'lai_s_cc', 'openness')
+photos_manual = merge(photos_meta, photos_manual, by.x='filename', by.y='original_file', all.y=TRUE)
+lai_manual = photos_manual %>%
+  gather('lai_method', 'manual', 3:6) %>%
+  select(-'filename', -'openness')
+
+photos_rc_in = "C:/Users/Cob/index/educational/usask/research/masters/data/hemispheres/19_149/clean/sized/LAI_rc_parsed.dat"
+photos_rc = read.csv(photos_rc_in, header=TRUE, na.strings = c("NA",""), sep=",")
+photos_rc$original_file = gsub("_r.JPG", ".JPG", photos_rc$picture)
+photos_rc = photos_rc %>% select('original_file', 'lai_no_cor', 'lai_s', 'lai_cc', 'lai_s_cc', 'openness')
+photos_rc = merge(photos_meta, photos_rc, by.x='filename', by.y='original_file', all.y=TRUE)
+lai_rc = photos_rc %>%
+  gather('lai_method', 'rc', 3:6) %>%
+  select(-'filename', -'openness')
+
+photos_rc_rings_in = "C:/Users/Cob/index/educational/usask/research/masters/data/hemispheres/19_149/clean/sized/LAI_rc_rings_parsed.dat"
+photos_rc_rings = read.csv(photos_rc_rings_in, header=TRUE, na.strings = c("NA",""), sep=",")
+photos_rc_rings$original_file = gsub("_r.JPG", ".JPG", photos_rc_rings$picture)
+photos_rc_rings = photos_rc_rings %>% select('original_file', 'lai_no_cor', 'lai_s', 'lai_cc', 'lai_s_cc', 'openness')
+photos_rc_rings = merge(photos_meta, photos_rc_rings, by.x='filename', by.y='original_file', all.y=TRUE)
+lai_rc_rings = photos_rc_rings %>%
+  gather('lai_method', 'rc_rings', 3:6) %>%
+  select(-'filename', -'openness')
+
+photos_nh_in = "C:/Users/Cob/index/educational/usask/research/masters/data/hemispheres/19_149/clean/sized/LAI_nh_parsed.dat"
+photos_nh = read.csv(photos_nh_in, header=TRUE, na.strings = c("NA",""), sep=",")
+photos_nh$original_file = gsub("_r.JPG", ".JPG", photos_nh$picture)
+photos_nh = photos_nh %>% select('original_file', 'lai_no_cor', 'lai_s', 'lai_cc', 'lai_s_cc', 'openness')
+photos_nh = merge(photos_meta, photos_nh, by.x='filename', by.y='original_file', all.y=TRUE)
+lai_nh = photos_nh %>%
+  gather('lai_method', 'nh', 3:6) %>%
+  select(-'filename', -'openness')
+
+photos_nh_rings_in = "C:/Users/Cob/index/educational/usask/research/masters/data/hemispheres/19_149/clean/sized/LAI_nh_rings_parsed.dat"
+photos_nh_rings = read.csv(photos_nh_rings_in, header=TRUE, na.strings = c("NA",""), sep=",")
+photos_nh_rings$original_file = gsub("_r.JPG", ".JPG", photos_nh_rings$picture)
+photos_nh_rings = photos_nh_rings %>% select('original_file', 'lai_no_cor', 'lai_s', 'lai_cc', 'lai_s_cc', 'openness')
+photos_nh_rings = merge(photos_meta, photos_nh_rings, by.x='filename', by.y='original_file', all.y=TRUE)
+lai_nh_rings = photos_nh_rings %>%
+  gather('lai_method', 'nh_rings', 3:6) %>%
+  select(-'filename', -'openness')
+
+
+
+synth_lai_in = "C:/Users/Cob/index/educational/usask/research/masters/data/lidar/synthetic_hemis/opt/poisson/LAI_parsed.dat"
+synth_lai = read.csv(synth_lai_in, header=TRUE, na.strings = c("NA",""), sep=",")
+synth_lai = synth_lai %>% select('picture', 'lai_no_cor', 'lai_s', 'lai_cc', 'lai_s_cc', 'openness')
+
+synth_meta_in = "C:/Users/Cob/index/educational/usask/research/masters/data/lidar/synthetic_hemis/opt/poisson/hemimetalog.csv"
+synth_meta = read.csv(synth_meta_in, header=TRUE, na.strings = c("NA",""), sep=",")
+synth_meta = synth_meta %>% select('id', 'file_name', 'poisson_radius_m', 'optimization_scalar')
+
+synth = merge(synth_meta, synth_lai, by.x='file_name', by.y='picture', all.x=TRUE)
+lai_synth = synth %>%
+  gather('lai_method', 'lai_synth', 5:8) %>%
+  select(-'openness')
+
+lai = merge(lai_synth, lai_manual, by=c('id', 'lai_method'), all.x=TRUE)
+lai = merge(lai, lai_rc, by=c('id', 'lai_method'), all.x=TRUE)
+lai = merge(lai, lai_rc_rings, by=c('id', 'lai_method'), all.x=TRUE)
+lai = merge(lai, lai_nh, by=c('id', 'lai_method'), all.x=TRUE)
+lai = merge(lai, lai_nh_rings, by=c('id', 'lai_method'), all.x=TRUE)
+
+lai = lai %>%
+  gather('threshold_method', 'lai_photo', 7:11) %>%
+  mutate(lai_error = lai_synth - lai_photo)
+
+rmse = function(difdata){
+  sqrt(sum(difdata^2, na.rm = TRUE))/sqrt(length(na.omit(difdata)))
+}
+# define rmse
+mae = function(difdata){
+  sum(abs(difdata), na.rm = TRUE)/length(na.omit(difdata))
+}
+
+
+lai_summary = lai %>%
+  group_by(poisson_radius_m, optimization_scalar, threshold_method, lai_method) %>%
+  summarise(lai_rmse = rmse(lai_error), lai_mae = mae(lai_error), lai_mean_bias = mean(lai_error, na.rm = TRUE), n = n())
+lai_summary$poisson_radius_m = as.factor(lai_summary$poisson_radius_m)
+
+# plots
+# rmse
+ggplot(lai_summary, aes(x=optimization_scalar, y=lai_rmse, color=poisson_radius_m)) +
+  facet_grid(lai_method ~ threshold_method) +
+  geom_point() +
+  geom_line() +
+  ylim(0, 2)
+
+# mb
+ggplot(lai_summary, aes(x=optimization_scalar, y=lai_mean_bias, color=poisson_radius_m)) +
+  facet_grid(lai_method ~ threshold_method) +
+  geom_point() +
+  geom_line()
+
+# all = merge(synth, photos, by='id', all.x=TRUE, suffixes=c("_synth", "_manual"))
+
+
+# BOOKMAKR -- everything below this is old hat...
 
 # plot
 # RMSE
@@ -82,8 +254,6 @@ ggplot(synth_photos_da, aes(x=th_hold_las, y=mean_bias_lai, color=os, shape=grou
   geom_line() +
   labs(title="Mean Bias of LAI across point size, threshold, and methods", x="Synthetic threshold (1-255)", y="Mean Bias of LAI (LiDAR - Photo)", color="Synthetic point size", shape="LAI method")
   
-
-
 
 
 synth_photos_da$th_hold_las = as.factor(synth_photos_da$th_hold_las)
