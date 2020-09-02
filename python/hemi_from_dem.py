031b22f5ba34e0ccb2ac77c442577dfc47925173
Staines <jas600@usask.ca>
2020-09-02 12:49:09 -0600

    small changes only to file pointers, commenting out, etc.

--- python/hemi_from_dem.py
+++ python/hemi_from_dem.py
@@ -3,38 +3,39 @@ import numpy as np
 import laslib
 import rastools
 
-# build point list
-dem_in = 'C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\19_149\\19_149_snow_off\\OUTPUT_FILES\\DEM\\19_149_dem_res_1.00m.bil'
-# load dem
-dem = rastools.raster_load(dem_in)
-# list points where dem data exists
-pts_index = np.where(dem.data != dem.no_data)
-# convert to utm
-pts_utm = dem.T1 * pts_index
-# add all to df
-pts = pd.DataFrame({'x_utm11n': pts_utm[0],
-                    'y_utm11n': pts_utm[1],
-                    'z_m': dem.data[pts_index],
-                    'x_index': pts_index[1],
-                    'y_index': pts_index[1]})
-# add point id
-pts = pts.reset_index()
-pts.columns = ['id', 'x_utm11n', 'y_utm11n', 'z_m', 'x_index', 'y_index']
-
-# add flag for UF
-uf_plot = dem
-uf_plot.data = np.full((dem.rows, dem.cols), 0)
-uf_plot_dir = 'C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\site_library\\uf_1m.tiff'
-rastools.raster_save(uf_plot, uf_plot_dir, data_format='byte')
-site_poly = 'C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\site_library\\upper_forest_poly_UTM11N.shp'
-rastools.raster_burn(uf_plot_dir, site_poly, 1)
-uf_plot = rastools.raster_load(uf_plot_dir)
-
-pts = pts.assign(uf=uf_plot.data[pts_index].astype(bool))
-pts_dir = 'C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\site_library\\1m_dem_points.csv'
-pts.to_csv(pts_dir, index=False)
+# # build point list
+# dem_in = 'C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\19_149\\19_149_snow_off\\OUTPUT_FILES\\DEM\\19_149_dem_res_1.00m.bil'
+# # load dem
+# dem = rastools.raster_load(dem_in)
+# # list points where dem data exists
+# pts_index = np.where(dem.data != dem.no_data)
+# # convert to utm
+# pts_utm = dem.T1 * pts_index
+# # add all to df
+# pts = pd.DataFrame({'x_utm11n': pts_utm[0],
+#                     'y_utm11n': pts_utm[1],
+#                     'z_m': dem.data[pts_index],
+#                     'x_index': pts_index[1],
+#                     'y_index': pts_index[1]})
+# # add point id
+# pts = pts.reset_index()
+# pts.columns = ['id', 'x_utm11n', 'y_utm11n', 'z_m', 'x_index', 'y_index']
+#
+# # add flag for UF
+# uf_plot = dem
+# uf_plot.data = np.full((dem.rows, dem.cols), 0)
+# uf_plot_dir = 'C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\site_library\\uf_1m.tiff'
+# rastools.raster_save(uf_plot, uf_plot_dir, data_format='byte')
+# site_poly = 'C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\site_library\\upper_forest_poly_UTM11N.shp'
+# rastools.raster_burn(uf_plot_dir, site_poly, 1)
+# uf_plot = rastools.raster_load(uf_plot_dir)
+#
+# pts = pts.assign(uf=uf_plot.data[pts_index].astype(bool))
+# pts.to_csv(pts_dir, index=False)
 
 # build hemispheres
+# pts_dir = 'C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\site_library\\1m_dem_points.csv'
+pts_dir = 'C:\\Users\\jas600\\workzone\\data\\hemigen\\hemi_lookups\\1m_dem_points.csv'
 pts = pd.read_csv(pts_dir)
 # filter to upper forest
 pts = pts[pts.uf]
@@ -50,7 +51,7 @@ hemimeta.src_keep_class = 5
 hemimeta.poisson_sampling_radius = 0.15  # meters (for no poisson sampling, specify 0)
 
 # output file dir
-hemimeta.file_dir = "C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\synthetic_hemis\\opt\\poisson\\"
+hemimeta.file_dir = "C:\\Users\\jas600\\workzone\\data\\hemigen\\outputs\\"
 
 # max distance of points considered in image
 hemimeta.max_distance = 50  # meters
@@ -61,27 +62,29 @@ hemimeta.img_size = 10  # in inches
 hemimeta.img_resolution = 100  # pixels/inch
 
 
-# poisson sample point cloud (src_las_in)
-if (hemimeta.poisson_sampling_radius is None) or (hemimeta.poisson_sampling_radius == 0):
-    # skip poisson sampling
-    las_poisson_path = hemimeta.src_las_file
-    print("no Poisson sampling conducted")
-else:
-    if hemimeta.poisson_sampling_radius > 0:
-        # poisson sampling
-        las_poisson_path = hemimeta.src_las_file.replace('.las', '_poisson_' + str(hemimeta.poisson_sampling_radius) + '.las')
-        # laslib.las_poisson_sample(hemimeta.src_las_file, hemimeta.poisson_sampling_radius, classification=hemimeta.src_keep_class, las_out=las_poisson_path)  # takes 10 minutes
-    else:
-        raise Exception('hemimeta.poisson_sampling_radius should be a numeric >= 0 or None.')
+# # poisson sample point cloud (src_las_in)
+# if (hemimeta.poisson_sampling_radius is None) or (hemimeta.poisson_sampling_radius == 0):
+#     # skip poisson sampling
+#     las_poisson_path = hemimeta.src_las_file
+#     print("no Poisson sampling conducted")
+# else:
+#     if hemimeta.poisson_sampling_radius > 0:
+#         # poisson sampling
+#         las_poisson_path = hemimeta.src_las_file.replace('.las', '_poisson_' + str(hemimeta.poisson_sampling_radius) + '.las')
+#         # laslib.las_poisson_sample(hemimeta.src_las_file, hemimeta.poisson_sampling_radius, classification=hemimeta.src_keep_class, las_out=las_poisson_path)  # takes 10 minutes
+#     else:
+#         raise Exception('hemimeta.poisson_sampling_radius should be a numeric >= 0 or None.')
+#
+# # export las to hdf5
+# print("-------- Exporting to HDF5 --------")
+# hdf5_path = las_poisson_path.replace('.las', '.hdf5')
+# laslib.las_xyz_to_hdf5(las_poisson_path, hdf5_path, keep_class=hemimeta.src_keep_class)
 
-# export las to hdf5
-print("-------- Exporting to HDF5 --------")
-hdf5_path = las_poisson_path.replace('.las', '.hdf5')
-laslib.las_xyz_to_hdf5(las_poisson_path, hdf5_path, keep_class=hemimeta.src_keep_class)
+hdf5_path = 'C:\\Users\\jas600\\workzone\\data\\hemigen\\hemi_lookups\\19_149_snow_off_classified_merged_poisson_0.15.hdf5'
 
 hemimeta.id = pts.id
-hemimeta.origin = np.array([pts.x_utmn11,
-                            pts.y_utmn11,
+hemimeta.origin = np.array([pts.x_utm11n,
+                            pts.y_utm11n,
                             pts.z_m + hemi_m_above_ground]).swapaxes(0, 1)
 
 
