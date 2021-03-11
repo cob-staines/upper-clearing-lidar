70fab5f77b5041546802e3d39ba3cbb70e7b8569
Staines <jas600@usask.ca>
2021-03-11 13:04:40 -0700

    corrected function calls

--- python/vox_045_050_052_config.py
+++ python/vox_045_050_052_config.py
@@ -6,12 +6,15 @@ voxList = ['C:\\Users\\jas600\\workzone\\data\\ray_sampling\\sources\\19_045_all
            'C:\\Users\\jas600\\workzone\\data\\ray_sampling\\sources\\19_050_all_WGS84_utm11N_19_050_r0.25_vox.h5',
            'C:\\Users\\jas600\\workzone\\data\\ray_sampling\\sources\\19_052_all_WGS84_utm11N_19_052_r0.25_vox.h5']
 
-# vox_out = 'C:\\Users\\jas600\\workzone\\data\\ray_sampling\\sources\\045_050_052_combined_WGS84_utm11N_r0.25_vox.h5'
-vox_out = 'C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\ray_sampling\\sources\\045_050_052_combined_WGS84_utm11N_r0.25_vox.h5'
+vox_out = 'C:\\Users\\jas600\\workzone\\data\\ray_sampling\\sources\\045_050_052_combined_WGS84_utm11N_r0.25_vox.h5'
+# vox_out = 'C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\ray_sampling\\sources\\045_050_052_combined_WGS84_utm11N_r0.25_vox.h5'
 z_slices = 8
 
 # BUILD VOX
 # combine voxel spaces
-vox = lrs.load_vox_meta(vox_out, load_data=False, load_post=True)
+lrs.vox_addition(voxList, vox_out, z_slices)
+vox = lrs.load_vox_meta(vox_out, load_data=False, load_post=False)
 # calculate prior
-# lrs.beta_lookup_prior_calc(vox, z_slices, agg_sample_length=None)
+lrs.beta_lookup_prior_calc(vox, z_slices, agg_sample_length=None)
+# combine voxel spaces
+vox = lrs.load_vox_meta(vox_out, load_data=False, load_post=True)
