c0e57fc7d7c00a2805b311158553e8e97f2990d1
Staines <jas600@usask.ca>
2021-03-23 13:03:16 -0600

    fixed a few bugs in rshmeta run

--- python/vox_045_050_052_config.py
+++ python/vox_045_050_052_config.py
@@ -2,16 +2,16 @@ import numpy as np
 import las_ray_sampling as lrs
 config_id = "045_050_052_r0.25"
 
-# voxList = ['C:\\Users\\jas600\\workzone\\data\\ray_sampling\\sources\\19_045_all_WGS84_utm11N_19_045_r0.25_vox.h5',
-#            'C:\\Users\\jas600\\workzone\\data\\ray_sampling\\sources\\19_050_all_WGS84_utm11N_19_050_r0.25_vox.h5',
-#            'C:\\Users\\jas600\\workzone\\data\\ray_sampling\\sources\\19_052_all_WGS84_utm11N_19_052_r0.25_vox.h5']
+voxList = ['C:\\Users\\jas600\\workzone\\data\\ray_sampling\\sources\\19_045_all_WGS84_utm11N_19_045_r0.25_vox.h5',
+           'C:\\Users\\jas600\\workzone\\data\\ray_sampling\\sources\\19_050_all_WGS84_utm11N_19_050_r0.25_vox.h5',
+           'C:\\Users\\jas600\\workzone\\data\\ray_sampling\\sources\\19_052_all_WGS84_utm11N_19_052_r0.25_vox.h5']
+vox_out = 'C:\\Users\\jas600\\workzone\\data\\ray_sampling\\sources\\045_050_052_combined_WGS84_utm11N_r0.25_vox.h5'
 
-voxList = ['C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\ray_sampling\\sources\\19_045_all_WGS84_utm11N_19_045_r0.25_vox.h5',
-           'C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\ray_sampling\\sources\\19_050_all_WGS84_utm11N_19_050_r0.25_vox.h5',
-           'C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\ray_sampling\\sources\\19_052_all_WGS84_utm11N_19_052_r0.25_vox.h5']
+# voxList = ['C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\ray_sampling\\sources\\19_045_all_WGS84_utm11N_19_045_r0.25_vox.h5',
+#            'C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\ray_sampling\\sources\\19_050_all_WGS84_utm11N_19_050_r0.25_vox.h5',
+#            'C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\ray_sampling\\sources\\19_052_all_WGS84_utm11N_19_052_r0.25_vox.h5']
+# vox_out = 'C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\ray_sampling\\sources\\045_050_052_combined_WGS84_utm11N_r0.25_vox.h5'
 
-# vox_out = 'C:\\Users\\jas600\\workzone\\data\\ray_sampling\\sources\\045_050_052_combined_WGS84_utm11N_r0.25_vox.h5'
-vox_out = 'C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\ray_sampling\\sources\\045_050_052_combined_WGS84_utm11N_r0.25_vox.h5'
 z_slices = 8
 
 # BUILD VOX
