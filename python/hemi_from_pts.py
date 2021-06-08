e2c4a97f83ab26e16ae53525f1606bac41050269
Staines <jas600@usask.ca>
2021-06-07 20:35:14 -0600

    paths

--- python/hemi_from_pts.py
+++ python/hemi_from_pts.py
@@ -3,15 +3,15 @@ import numpy as np
 import laslib
 import os
 
+#
+# batch_dir = 'C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\synthetic_hemis\\batches\\uf_1m_pr0_os1.06_snow_off_dem_offset.25\\'
+# las_in = "C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\19_149\\19_149_las_proc\\OUTPUT_FILES\\LAS\\19_149_las_proc_classified_merged.las"
+# pts_in = "C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\synthetic_hemis\\hemi_grid_points\\mb_65_1m\\1m_dem_points_mb_15.csv"
 
-batch_dir = 'C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\synthetic_hemis\\batches\\mb_15_1m_pr_.15_os_10\\'
-las_in = "C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\19_149\\19_149_las_proc\\OUTPUT_FILES\\LAS\\19_149_las_proc_classified_merged.las"
-pts_in = "C:\\Users\\Cob\\index\\educational\\usask\\research\\masters\\data\\lidar\\synthetic_hemis\\hemi_grid_points\\mb_65_1m\\1m_dem_points_mb_15.csv"
 
-
-# batch_dir = 'C:\\Users\\jas600\\workzone\\data\\hemigen\\uf_1m_pr0_os.88\\'
-# las_in = "C:\\Users\\jas600\\workzone\\data\\hemigen\\hemi_lookups\\19_149_las_proc_classified_merged.las"
-# pts_in = 'C:\\Users\\jas600\\workzone\\data\\hemigen\\hemi_lookups\\dem_r.25_point_ids_1m subset.csv'
+batch_dir = 'C:\\Users\\jas600\\workzone\\data\\hemigen\\uf_1m_pr0_os1.06_snow_off_dem_offset.25\\'
+las_in = "C:\\Users\\jas600\\workzone\\data\\ray_sampling\\sources\\19_149\\19_149_las_proc_classified_merged.las"
+pts_in = 'C:\\Users\\jas600\\workzone\\data\\hemi_grid_points\\mb_65_r.25m_snow_off_offset.25\\dem_r.25_point_ids_1m subset.csv'
 
 # create batch dir if does not exist
 if not os.path.exists(batch_dir):
@@ -28,7 +28,7 @@ hemimeta = laslib.HemiMetaObj()
 las_day = 19_149
 hemimeta.src_las_file = las_in
 hemimeta.src_keep_class = [1, 5]  # range of classes or single class ([1, 5] passes all classes within 1-5)
-hemimeta.poisson_sampling_radius = .15  # meters (for no poisson sampling, specify 0)
+hemimeta.poisson_sampling_radius = 0  # meters (for no poisson sampling, specify 0)
 
 # output file dir
 hemimeta.file_dir = batch_dir + "outputs\\"
@@ -54,14 +54,14 @@ else:
     if hemimeta.poisson_sampling_radius > 0:
         # poisson sampling
         las_poisson_path = hemimeta.src_las_file.replace('.las', '_poisson_' + str(hemimeta.poisson_sampling_radius) + '.las')
-        laslib.las_poisson_sample(hemimeta.src_las_file, hemimeta.poisson_sampling_radius, classification=hemimeta.src_keep_class, las_out=las_poisson_path)  # takes 10 minutes
+        # laslib.las_poisson_sample(hemimeta.src_las_file, hemimeta.poisson_sampling_radius, classification=hemimeta.src_keep_class, las_out=las_poisson_path)  # takes 10 minutes
     else:
         raise Exception('hemimeta.poisson_sampling_radius should be a numeric >= 0 or None.')
 
 # export las to hdf5
 print("-------- Exporting to HDF5 --------")
 hdf5_path = las_poisson_path.replace('.las', '.hdf5')
-laslib.las_xyz_to_hdf5(las_poisson_path, hdf5_path, keep_class=hemimeta.src_keep_class)
+# laslib.las_xyz_to_hdf5(las_poisson_path, hdf5_path, keep_class=hemimeta.src_keep_class)
 
 hemimeta.id = pts.id
 hemimeta.origin = np.array([pts.x_utm11n,
